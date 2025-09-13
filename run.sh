2a8ed54fb6efd6c48a70229caf18b2e544280175
Efim Ishenin <me@efima.ru>
2025-09-13 23:39:59 +0700

    improved run script and added c++ list argumets testing file

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
-g++ $1 -o $1.out
-./$1.out
-rm -f $1.out
+
+# Check if filename is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <file.cpp> [<c++ options>]"
+  exit 1
+fi
+
+# Compile the C++ file
+g++ "$1" -o "${1%.cpp}.out"
+if [ $? -ne 0 ]; then
+  echo "Compilation failed."
+  exit 1
+fi
+
+# Run the executable with arguments
+./"${1%.cpp}.out" "${@:2}"
+
+# Cleanup
+rm -f "${1%.cpp}.out"
